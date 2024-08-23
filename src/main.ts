47006cfceaa4ad319e375b450e0a44c2bde0dc0a
Kimberley Antao <kimberley2609@gmail.com>
2024-08-23 23:57:32 +0100

    initial commit: added grid loyout and a function to dynamically create the grid

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './style.css'
+
+const generateGrid = () =>{
+    let container = document.querySelector<HTMLDivElement>('.inner-container');
+    
+    if(!container ){
+        throw new Error ("Encountered issue creating the main grid");
+    }
+
+    for(let i:number =0; i<8; i++){
+        container.innerHTML += '<div class="cell--column"></div>';
+    }
+    let column = document.querySelectorAll<HTMLDivElement>('.cell--column');
+
+    column.forEach((col)=>{
+        for(let i:number =0; i<8; i++){
+        col.innerHTML += '<div class="cell--row"></div>';
+        }
+    })
+
+   
+
+}
+
+generateGrid();
+
